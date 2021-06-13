0629ffe029ced599661352bd2bbc24e243705bad
tejasgoel786 <tejas.goel786@gmail.com>
2021-06-13 18:13:19 +0000

    mae change in dev branch

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to sdsadsa<a href="https://nextjs.org">Next.js!</a>
+          made changes<a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
